b0347c2e14d17debc37ea9d8524d344ecf5e051a
dunbar-brett <dunbar.brett@gmail.com>
2021-03-12 14:22:22 -0800

    first commit

---
+++ src/main/java/com/brettdunbar/crowdStreetInvestorApi/CrowdStreetInvestorApiApplication.java
@@ -0,0 +1,13 @@
+package com.brettdunbar.crowdStreetInvestorApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdStreetInvestorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrowdStreetInvestorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brettdunbar/crowdStreetInvestorApi/CrowdStreetInvestorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brettdunbar.crowdStreetInvestorApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowdStreetInvestorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
