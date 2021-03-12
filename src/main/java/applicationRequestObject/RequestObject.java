8220f7ffbb0d401a9ceead7d2418c2cc0ed0ddd7
dunbar-brett <dunbar.brett@gmail.com>
2021-03-12 14:48:30 -0800

    created mock data for start of api

---
+++ src/main/java/applicationRequestObject/RequestObject.java
@@ -0,0 +1,77 @@
+package applicationRequestObject;
+import java.util.Date;
+import java.util.UUID;
+
+public class RequestObject {
+    private UUID requestId;
+    private String body;
+    private String status; // TODO: status should likely be an enum
+    private String detail;
+    private Date createdTimeStamp;
+    private Date lastUpdatedTimeStamp;
+
+    public RequestObject(UUID requestId,
+                         String body,
+                         String status,
+                         String detail,
+                         Date createdTimeStamp,
+                         Date lastUpdatedTimeStamp) {
+        this.requestId = requestId;
+        this.body = body;
+        this.status = status;
+        this.detail = detail;
+        this.createdTimeStamp = createdTimeStamp;
+        this.lastUpdatedTimeStamp = lastUpdatedTimeStamp;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public Date getCreatedTimeStamp() {
+        return createdTimeStamp;
+    }
+
+    public void setCreatedTimeStamp(Date createdTimeStamp) {
+        this.createdTimeStamp = createdTimeStamp;
+    }
+
+    public Date getLastUpdatedTimeStamp() {
+        return lastUpdatedTimeStamp;
+    }
+
+    public void setLastUpdatedTimeStamp(Date lastUpdatedTimeStamp) {
+        this.lastUpdatedTimeStamp = lastUpdatedTimeStamp;
+    }
+
+    public UUID getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(UUID requestId) {
+        this.requestId = requestId;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    // TODO: Maybe implement an equals method
+
+}
---
+++ src/main/java/dataStore/MockRequestObjectStore.java
@@ -0,0 +1,45 @@
+package dataStore;
+
+import applicationRequestObject.RequestObject;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public class MockRequestObjectStore {
+
+    private static final List<RequestObject> REQUEST_OBJECT_LIST = new ArrayList<>();
+
+    static {
+        REQUEST_OBJECT_LIST
+            .add(new RequestObject(
+                UUID.randomUUID(),
+                "some random text that should be json",
+                "PROCESSED",
+                "something details",
+                new Date(),
+                new Date()
+            ));
+        REQUEST_OBJECT_LIST
+            .add(new RequestObject(
+                    UUID.randomUUID(),
+                    "some random text that should be json",
+                    "PROCESSED",
+                    "something details",
+                    new Date(),
+                    new Date()
+            ));
+        REQUEST_OBJECT_LIST
+            .add(new RequestObject(
+                    UUID.randomUUID(),
+                    "some random text that should be json",
+                    "PROCESSED",
+                    "something details",
+                    new Date(),
+                    new Date()
+            ));
+    }
+}
