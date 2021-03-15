8899518e7c5df8b612b71cb32c2065194e73d285
dunbar-brett <dunbar.brett@gmail.com>
2021-03-14 18:37:33 -0700

    additional comments

--- src/main/java/com/brettdunbar/crowdStreetInvestorApi/applicationRequestObject/RequestObjectDataAccessService.java
+++ src/main/java/com/brettdunbar/crowdStreetInvestorApi/applicationRequestObject/RequestObjectDataAccessService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Date;
 import java.util.List;
 
+// instead this should normally be repository for a Database 
 @Repository
 public class RequestObjectDataAccessService {
 
--- src/main/java/com/brettdunbar/crowdStreetInvestorApi/dataStore/MockRequestObjectStore.java
+++ src/main/java/com/brettdunbar/crowdStreetInvestorApi/dataStore/MockRequestObjectStore.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.util.*;
 
 
-
+// just a mock data store to handle objects in memory
 @Repository
 public class MockRequestObjectStore {
 
